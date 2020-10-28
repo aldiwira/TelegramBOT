a9da1d0f050c730dac2442f2e75ddeb50ac0792e
aldi wirawan <aldiwira.aw@gmail.com>
2020-10-28 19:14:10 +0700

    Add 5 Recent Earthquake

--- scene/earthquake.js
+++ scene/earthquake.js
@@ -4,6 +4,7 @@ const Extra = require("telegraf/extra");
 const Axios = require("axios").default;
 const moment = require("moment");
 const { xml2js } = require("xml-js");
+const { reply } = require("telegraf/scenes/base");
 
 moment.locale("id");
 const dateNow = moment().format("dddd, Do MMMM YYYY");
@@ -14,7 +15,7 @@ const xmlParser = (data) => {
   return xml2js(data, { compact: true });
 };
 
-const homeList = ["Gempa hari ini", "Home"];
+const homeList = ["Gempa hari ini", "Gempa terkini", "Home"];
 
 eqScene.enter(({ reply }) => {
   return reply(
@@ -35,7 +36,23 @@ eqScene.hears(homeList[0], async (ctx) => {
     await ctx.replyWithAnimation({ url: "https://data.bmkg.go.id/eqmap.gif" });
     const resText = `Waktu Kejadian \nTanggal : ${infoGempa.Tanggal._text}\nJam : ${infoGempa.Jam._text}\n\nKoordinat\nLintang : ${location.Lintang}\nBujur : ${location.Bujur}\n\nData Gempa \nKekuatan Gempa : ${infoGempa.Magnitude._text}\nKedalaman Gempa : ${infoGempa.Kedalaman._text}\n${infoGempa.Potensi._text}\n\nRincian Lokasi : \n${infoGempa.Wilayah1._text}\n${infoGempa.Wilayah2._text}\n${infoGempa.Wilayah3._text}\n${infoGempa.Wilayah4._text}\n${infoGempa.Wilayah5._text}`;
     await ctx.reply(resText);
-    await backHandler(ctx);
+  });
+});
+
+eqScene.hears(homeList[1], async (ctx) => {
+  await Axios.get(url + "gempaterkini.xml").then(async (datas) => {
+    const terkini = xmlParser(datas.data).Infogempa.gempa;
+    await ctx.reply("5 Gempa terkini");
+    for (let i = 0; i < 5; i++) {
+      const item = terkini[i];
+      const location = {
+        lintang: String(item.point.coordinates._text).split(",")[0],
+        bujur: String(item.point.coordinates._text).split(",")[1],
+      };
+      await ctx.replyWithLocation(location.bujur, location.lintang);
+      const resText = `Waktu Kejadian \nTanggal : ${item.Tanggal._text}\nJam : ${item.Jam._text}\n\nKoordinat\nLintang : ${location.lintang}\nBujur : ${location.bujur}\n\nData Gempa \nKekuatan Gempa : ${item.Magnitude._text}\nKedalaman Gempa : ${item.Kedalaman._text}\nWilayah : ${item.Wilayah._text}`;
+      await ctx.reply(resText);
+    }
   });
 });
 
